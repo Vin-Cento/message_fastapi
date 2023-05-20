5791297a0d3c1a61980af1f6d648f691bbf04f27
Vincent <vincentliu362@gmail.com>
2023-05-20 14:18:32 -0500

    chore: init auth with some test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,5 +6,15 @@ def signup(username, password):
     res = requests.post(f"{ENDPOINT}", json=payload)
     return res
 
-assert signup("vinny2", "vinny2").status_code == 406
+def login(username, password):
+    ENDPOINT = "http://localhost:8004/login"
+    payload = {"username": username, "password": password}
+    res = requests.post(f"{ENDPOINT}", json=payload)
+    return res
+
+# sign up with new user
+assert signup("new_user", "password").status_code == 200
+assert login("new_user", "password").status_code == 200
+
+# sign up with username already taken
 assert signup("vinny1", "vinny1").status_code == 406
